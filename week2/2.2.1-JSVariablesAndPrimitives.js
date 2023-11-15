422e2a0de8cf2c7a4065c2a7c984d24ed4079907
Greg Carter <greg.a.carter1@gmail.com>
2023-11-15 14:47:34 -0500

    JS Variables and Primitives Part 4

---
+++ week2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,28 @@
+const { userInfo } = require("os");
+
+// create a constant variable (const)
+const name = "David",
+// create a variable that can be reassigned (let)
+let country = 'us';
+// create three variables and assign them values of different data types (=)
+let country = 'us';
+let 4 = 2+2;
+let this = true
+// print the types of two variables that reference two different data types (typeof)
+console.log(typof country);
+console.log(typeof 4);
+
+// TIP: console.log() prints a value in the console/terminal
+
+// change the value referenced by a variable (dynamic typing)
+let country = 42;
+// print the type of the variable you just changed (typeof)
+console.log(typeof country);
+// create variables and give them values to complete the sentences that will print  below.
+const greeting = "Hello, my name is" +
+let name = "Greg," + "and I am learning" + let language ="javascript." + "I have been practicing for only" + let number =2 + "weeks, so my status as a master is" + false;
+// Which variables need to be created?
+name, language, number, and tue/false.
+// What type of data needs to go in each variable?
+Name is string, number is number, language is string, and true/false is Boolean.
+console.log("Hello, my name is " + {add a name} + ", and I am learning " + {add a language} + ". I have been practicing for only " +  {add a number} + " weeks, so my status as a master is: " + {true or false} + ".");
---
+++ week2/2.2.3-JSMorePrimitives.js
@@ -0,0 +1,28 @@
+// Create a variable "timeOfDay" and assign it a string
+let timeOfDay = "evening"
+// Create a variable "greeting" that references a template literal
+// Use "timeOfDay" in the template literal to create a message like "Good morning!" or "Good evening!"
+let greeting= "Good ${timeOfDay}";
+// Print "greeting"
+console.log(greeting);
+// Create a new variable, but do not assign it a value
+let newVariable
+// Print the new variable and its type
+console.log(newVariable);
+console.log(typeof newVariable);
+// What type should we expect?
+
+// Assign the variable a value that indicates the variable is purposely blank
+let name = null;
+// What value should we assign?
+
+// Print the variable and its type again
+console.log(newVariable);
+// What type should we expect?
+console.log(typeof newVariable);
+// Try to print a variable that does not exist
+
+// What should we expect to print in the CLI?
+
+// Print "greeting" again
+// Will this line run?
