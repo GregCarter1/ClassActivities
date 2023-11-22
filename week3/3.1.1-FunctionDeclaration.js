d7eb3136894e5e2f1ac76557c481fd0d5bb593ca
Greg Carter <greg.a.carter1@gmail.com>
2023-11-21 19:57:19 -0500

    add sect 3.1.1 through 3.1.4

---
+++ week3/3.1.1-FunctionDeclaration.js
@@ -0,0 +1,32 @@
+//TODO: using the function expressions below, refactor them into function declarations
+//
+//function declaration
+function greet() {
+  console.log("Hello!");
+}
+
+
+//const threeModTwo = function() {
+  //console.log(3 % 2);
+//};
+function threeModTwo() {
+  console.log(3 % 2);
+}
+//function declaration
+
+//let age = 18;
+//const checkDrivingAge = function() {
+  //if (age >= 16) {
+    //console.log(true);
+  //} else {
+    //console.log(false);
+  //}
+//};
+
+function checkDrivingAge() {
+  if (age>= 16) {
+    console.log(true);
+  } else {
+    console.log(false);
+    }
+}
