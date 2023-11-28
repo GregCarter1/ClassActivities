701708bb1753e7fa88af2090d6f0ac9949b096b5
Greg Carter <greg.a.carter1@gmail.com>
2023-11-27 22:04:26 -0500

    add activity4.1.1 abd 4.1.2

--- week3/3.1.7-ArrowSyntax.js
+++ week3/3.1.7-ArrowSyntax.js
@@ -3,21 +3,28 @@ function myNumberFunction() {
   return ((2 ** 2 + 3) % 4) * 14;
 }
 
-function greet(name) {
-  return `Hello, ${name}`;
-}
 
-const timeOfDayGreet = function(name, timeOfDay) {
-  return `Hello, ${name}, good ${timeOfDay}`;
-};
+// function greet(name) {
+//   return `Hello, ${name}`;
+// }
+let greet = name => 'Hello, ${name}';
 
-function tripleAndHalf(num) {
-  let triple = num * 3;
-  return triple / 2;
-}
+// const timeOfDayGreet = function(name, timeOfDay) {
+//   return `Hello, ${name}, good ${timeOfDay}`;
+// };
+const timeOfDayGreet = (name, timeOfDay) => 'Hello, ${name}, good ${timeOfDay}';
 
-function sumTwoNumbers(num1, num2) {
-  let sum = num1 + num2;
-  console.log(`The sum of your numbers is ${sum}.`);
-  return sum;
-}
+// function tripleAndHalf(num) {
+//   let triple = num * 3;
+//   return triple / 2;
+// }
+
+let triple = tripleAndHalf(num * 3) => triple/2;
+
+// function sumTwoNumbers(num1, num2) {
+//   let sum = num1 + num2;
+//   console.log(`The sum of your numbers is ${sum}.`);
+//   return sum;
+// }
+
+let sum = num1 + num2
